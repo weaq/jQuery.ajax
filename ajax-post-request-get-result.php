fe840ab42146cb76dd56e5c37655b332df2f5fa2
weaq <30413309+weaq@users.noreply.github.com>
2022-08-24 14:54:08 +0700

    Update ajax-post-request-get-result.php

--- ajax-post-request-get-result.php
+++ ajax-post-request-get-result.php
@@ -1,44 +1,68 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-$(document).ready(function() {
-  $("#submit").click(function() {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
+	<title>Bootstrap Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    if(name==''||email==''||message=='') {
-      alert("Please fill all fields.");
-      return false;
-    }
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script>
+		$(document).ready(function() {
 
-    $.ajax({
-      type: "POST",
-      url: "submit.php",
-      data: {
-      name: name,
-      email: email,
-      message: message
-      },
-      cache: false,
-      success: function(result) {
-        $("#submit_result").html(result);
-        document.getElementById('submit').style.backgroundColor='green';
-        document.getElementById('submit').style.color='white';
-      },
-      error: function(xhr, status, error) {
-      console.error(xhr);
-      }
-    });
-  });
-});
-</script>
+			$("#form_login").submit(function(e) {
+				//prevent Default functionality
+				e.preventDefault();
+
+				var username = $("#username").val().trim();
+				var password = $("#password").val().trim();
+
+				if (username == '' || password == '') {
+					alert("Please fill all fields.");
+					return false;
+				}
+
+				var jsonObj = JSON.stringify({
+					"username": username,
+					"password": password
+				})
+
+				$.ajax({
+					type: "POST",
+					url: "http://localhost:3333/login",
+					data: jsonObj,
+					contentType: "application/json; charset=utf-8",
+					success: function(result) {
+						alert(result.status);
+					}
+				});
+
+			});
+		});
+	</script>
 </head>
-<body>
 
-<button id="submit">Send an HTTP POST request to a page and get the result back</button>
-<div id="submit_result"> </div>
+<body>
+	<div class="container mt-3">
+		<form id="form_login">
+			<div class="mb-3 mt-3">
+				<label for="username">Username:</label>
+				<input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
+			</div>
+			<div class="mb-3">
+				<label for="password">Password:</label>
+				<input type="password" class="form-control" id="password" placeholder="Enter password" name="password" autocomplete="true">
+			</div>
+			<div class="form-check mb-3">
+				<label class="form-check-label">
+					<input class="form-check-input" type="checkbox" name="remember"> Remember me
+				</label>
+			</div>
+			<button class="btn btn-primary">Submit</button>
+		</form>
+	</div>
 </body>
+
 </html>
